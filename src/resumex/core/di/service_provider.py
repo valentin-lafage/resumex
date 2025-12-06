9e970421894eaea23e73945f78323d7398482010
Valentin Lafage <vlafage.dev@free.fr>
2025-12-06 11:20:09 +0100

    feat: add json service

--- src/resumex/core/di/service_provider.py
+++ src/resumex/core/di/service_provider.py
@@ -1,5 +1,5 @@
 from resumex.core.di import Provider
-from resumex.core.services import FileService, TemplateService
+from resumex.core.services import FileService, JsonService, TemplateService
 
 
 class ServiceProvider(Provider):
@@ -9,10 +9,15 @@ class ServiceProvider(Provider):
         return self._file_service
 
     @property
+    def json_service(self) -> JsonService:
+        return self._json_service
+
+    @property
     def template_service(self) -> TemplateService:
         return self._template_service
 
     def __init__(self):
         super().__init__()
         self._file_service = FileService()
-        self._template_service = TemplateService(self._file_service)
+        self._json_service = JsonService(self._file_service)
+        self._template_service = TemplateService(self._file_service, self._json_service)
--- src/resumex/core/services/__init__.py
+++ src/resumex/core/services/__init__.py
@@ -1,2 +1,3 @@
 from .file_service import FileService
+from .json_service import JsonService
 from .template_service import TemplateService
