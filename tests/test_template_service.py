e635e2b4ba4ba0dd0167915c4037541fa087292b
Valentin Lafage <vlafage.dev@free.fr>
2025-12-11 13:38:57 +0100

    test: use backup service
    Signed-off-by: Valentin Lafage <vlafage.dev@free.fr>

--- tests/test_template_service.py
+++ tests/test_template_service.py
@@ -1,15 +1,13 @@
-from resumex.core.services import TemplateService
 from resumex.templates import Template
 
 
-def test_default_rendering(
-    tmp_path, json_service_mock, default_result, file_service_mock, mocker
-):
+def test_default_rendering(tmp_path, template_service, default_result, mocker):
+    """
+    Should produce a tex file matching the default template.
+    """
     out_path = tmp_path.joinpath("default.tex")
-
-    service = TemplateService(file_service_mock, json_service_mock)
     mocker.patch.object(Template.DEFAULT, "get_out_path", return_value=out_path)
-    service.render(template=Template.DEFAULT)
+    template_service.render()
 
     assert out_path.exists()
     assert default_result == out_path.read_text()
